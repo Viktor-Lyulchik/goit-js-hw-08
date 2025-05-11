5ed566aa636acb0e37e26bcf782ee7a7faf8fd67
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2025-05-11 20:34:41 +0300

    add solution task gallery

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# goit-js-hw-07
+# goit-js-hw-08
 
-https://github.com/Viktor-Lyulchik/goit-js-hw-07
-https://viktor-lyulchik.github.io/goit-js-hw-07/
+https://github.com/Viktor-Lyulchik/goit-js-hw-08
+https://viktor-lyulchik.github.io/goit-js-hw-08/
--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const textInput = document.querySelector('input#name-input');
-const spanOutput = document.querySelector('span#name-output');
-
-textInput.addEventListener('input', event => {
-  const inputText = String(event.currentTarget.value).trim();
-  spanOutput.textContent = inputText === '' ? 'Anonymous' : inputText;
-});
